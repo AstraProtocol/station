eccbdbc9a54380e82a5e95bf983c0db6b43e6911
Nhan Hoang Trong <nhan.hoang2@tiki.vn>
2022-05-31 11:42:05 +0700

    chore: update path matches vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,49 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import svgrPlugin from 'vite-plugin-svgr'
-import { resolve } from "path";
-import { compilerOptions } from "./tsconfig.json";
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import svgrPlugin from 'vite-plugin-svgr';
+import { resolve } from 'path';
+import { compilerOptions } from './tsconfig.json';
+import react from '@vitejs/plugin-react';
 
 const alias = Object.entries(compilerOptions.paths).reduce((acc, [key, [value]]) => {
-	const aliasKey = key.substring(0, key.length - 2);
-	const path = value.substring(0, value.length - 2);
-	return {
-		...acc,
-		[aliasKey]: resolve(__dirname, path),
-	};
+  const aliasKey = key.substring(0, key.length - 2);
+  const path = value.substring(0, value.length - 2);
+  return {
+    ...acc,
+    [aliasKey]: resolve(__dirname, path),
+    React: 'react',
+  };
 }, {});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// This changes the out put dir from dist to build
-	// comment this out if that isn't relevant for your project
-	build: {
-		outDir: "build",
-	},
-	plugins: [
-		reactRefresh(),
-		svgrPlugin({
-			svgrOptions: {
-				icon: true,
-				// ...svgr options (https://react-svgr.com/docs/options/)
-			},
-		}),
-	],
-	resolve: {
-		alias,
-	},
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: 'build',
+  },
+  plugins: [
+    react(),
+    reactRefresh(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+  ],
+  resolve: {
+    alias,
+  },
+  define: {
+    'process.env': process.env,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
 });
